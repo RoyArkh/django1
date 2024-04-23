a6728c31e56a45a8a10331e227a1471686027ffb
SupRoya <rarkhmammadova22@ku.edu.tr>
2024-04-23 11:44:22 +0300

    templates configurations

--- django1.1/dj11/blog/views.py
+++ django1.1/dj11/blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {'author': 'Dr. Roya',
+     'title': 'Post 1',
+     'content': 'lorem ipsum 1',
+     'date': 'Today'},
+    {'author': 'Jane Doe',
+     'title': 'Post 2',
+     'content': 'lorem ipsum 2',
+     'date': 'Today 2'}
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Us'})
