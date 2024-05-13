b6e4f8c3fa38e65a5a095eea161e0f15c252ca33
SupRoya <rarkhmammadova22@ku.edu.tr>
2024-05-13 16:25:53 +0300

    Supporting multiple tag search

--- django1.1/dj11/blog/urls.py
+++ django1.1/dj11/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('filter/<str:tag>/', views.search_tag, name='blog-tagged'),
+    path('filter/<str:tags>/', views.search_tags, name='search-tags'),
     path('filter/', views.home, name='blog-home'),
 ]
 
--- django1.1/dj11/blog/views.py
+++ django1.1/dj11/blog/views.py
@@ -9,23 +9,43 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 
-def search_tag(request, tag):
-    tag_obj = Tag.objects.filter(name=tag).first()
+# def search_tag(request, tag):
+#     tag_obj = Tag.objects.filter(name=tag).first()
 
-    if tag_obj:
-        tagged_posts = Post.objects.filter(tags=tag_obj)
-        context = {
-            'posts': tagged_posts,
-            'tag': tag,
-        }
-    else:
-        context = {
-            'posts': [],
-            'tag': tag,
-        }
+#     if tag_obj:
+#         tagged_posts = Post.objects.filter(tags=tag_obj)
+#         context = {
+#             'posts': tagged_posts,
+#             'tag': tag,
+#         }
+#     else:
+#         context = {
+#             'posts': [],
+#             'tag': tag,
+#         }
 
-    return render(request, 'blog/home.html', context)
+#     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About Us'})
+
+
+def search_tags(request, tags):
+    tag_names = tags.split('_')
+    tag_objs = Tag.objects.filter(name__in=tag_names)
+
+    if tag_objs:
+        tagged_posts = Post.objects.filter(tags__in=tag_objs).distinct()
+        context = {
+            'posts': tagged_posts,
+            'tags': tag_names,
+        }
+    else:
+        context = {
+            'posts': [],
+            'tags': tag_names,
+        }
+
+    return render(request, 'blog/home.html', context)
+
