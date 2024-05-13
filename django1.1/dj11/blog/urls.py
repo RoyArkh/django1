f45b5d55f5201567085f3bbfa550ced45b6241c0
SupRoya <rarkhmammadova22@ku.edu.tr>
2024-05-13 21:18:26 +0300

    tags front end and links

--- django1.1/dj11/blog/urls.py
+++ django1.1/dj11/blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('filter/<str:tags>/', views.search_tags, name='search-tags'),
-    path('filter/', views.home, name='blog-home'),
+    path('filter/', views.home, name='blog-home-filter'),
 ]
 
